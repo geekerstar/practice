774a643f1ffffa945f0824794d2a19c37f17f59c
geekerstar <247507792@qq.com>
2018-09-21 09:19:37 +0800

    Initial commit

---
+++ src/com/geekerstar/al01/dividevyzero.java
@@ -0,0 +1,10 @@
+package com.geekerstar.al01;
+
+public class dividevyzero {
+    public static void main(String[] args) {
+
+        System.out.println(1.0/0.0);
+        System.out.println(1/0);
+
+    }
+}
